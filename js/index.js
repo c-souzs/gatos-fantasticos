d0a5b497f75f1b31c519395dfb71730c33cfcdda
Caio Souza <souzsdev@gmail.com>
2022-05-05 18:44:39 -0300

    Faq - accordion list

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ import MenuMobile from "./module/menu-mobile.js";
 import menuDropDown from "./module/menu-drop-down.js";
 import animacaoAoScroll from './module/animacao-ao-scroll.js';
 import navegacaoTab from './module/navegacao-tab.js';
+import accordionListFaq from './module/accordion-list-faq.js';
 
 menuDropDown();
 animacaoAoScroll();
 navegacaoTab();
+accordionListFaq();
 // Menu mobile com classe
 const btnMenuMobile = document.querySelector('[data-menu-mobile="btn"]');
 const listaMenuMobile = document.querySelector('[data-menu-mobile="lista"]');
