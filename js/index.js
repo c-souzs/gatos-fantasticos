2f99a0541ee87a73b9de1f48d7fea2cb9aec7de4
Caio Souza <souzsdev@gmail.com>
2022-05-05 18:10:22 -0300

    Animação ao scroll

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import MenuMobile from "./module/menu-mobile.js";
 import menuDropDown from "./module/menu-drop-down.js";
+import animacaoAoScroll from './module/animacao-ao-scroll.js';
 
 menuDropDown();
+animacaoAoScroll();
 // Menu mobile com classe
 const btnMenuMobile = document.querySelector('[data-menu-mobile="btn"]');
 const listaMenuMobile = document.querySelector('[data-menu-mobile="lista"]');
